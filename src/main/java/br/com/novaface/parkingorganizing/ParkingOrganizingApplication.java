a629521b7b7c6ad95b407a2efe46891e00ba8967
vastidev <Vastiane1@gmail.com>
2023-06-19 07:33:32 -0300

    implementa api

--- src/main/java/br/com/novaface/parkingorganizing/ParkingOrganizingApplication.java
+++ src/main/java/br/com/novaface/parkingorganizing/ParkingOrganizingApplication.java
@@ -2,9 +2,18 @@ package br.com.novaface.parkingorganizing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ParkingOrganizingApplication {
+	@GetMapping
+	public String getHomeTeste(){
+		return "Parking organize - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingOrganizingApplication.class, args);
