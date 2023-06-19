a32ee65c5559fade58b670315d5010dd326ae592
vastidev <Vastiane1@gmail.com>
2023-06-19 05:26:14 -0300

    inicia o projeto

---
+++ src/main/java/br/com/novaface/parkingorganizing/ParkingOrganizingApplication.java
@@ -0,0 +1,13 @@
+package br.com.novaface.parkingorganizing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingOrganizingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingOrganizingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/novaface/parkingorganizing/ParkingOrganizingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.novaface.parkingorganizing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingOrganizingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
