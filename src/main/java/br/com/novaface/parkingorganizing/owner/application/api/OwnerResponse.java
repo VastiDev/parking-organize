5133308d86b20395b4faeba5564d1ff3979db108
vastidev <Vastiane1@gmail.com>
2023-06-20 03:50:58 -0300

    implementa request e response

--- src/main/java/br/com/novaface/parkingorganizing/owner/application/api/OwnerResponse.java
+++ src/main/java/br/com/novaface/parkingorganizing/owner/application/api/OwnerResponse.java
@@ -1,4 +1,9 @@
 package br.com.novaface.parkingorganizing.owner.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+@Value
 public class OwnerResponse {
+    private UUID idOwner;
 }
